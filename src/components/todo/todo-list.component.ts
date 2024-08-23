0d1ad9b7da0ae81b3c86159531468cec5da24d8f
TomaszKasowicz <tomasz.kasowicz@gmail.com>
2024-08-23 15:40:53 +0200

    Update description

--- src/components/todo/todo-list.component.ts
+++ src/components/todo/todo-list.component.ts
@@ -1,6 +1,30 @@
 import { Component } from "@angular/core";
 import { RouterLink } from "@angular/router";
 
+
+/**
+ * Implement simple todo list with toggling Done/Not Done
+ * 
+ * FORM:
+ * - Implement Input using Reactive Forms
+ * - Task Name should be REQUIRED
+ * - Task Name should have MINIMUM 3 CHARACTERS
+ * - Add button should be enabled on when Task Name is VALID
+ * 
+ * TODO SECTION:
+ * - Todo section should be visible only when there is at least one task
+ * 
+ * COUNTER:
+ * - Counter should calculate number of remaining tasks out of all tasks
+ * 
+ * TASK LIST:
+ * - Iterate over "items"
+ * - When NOT DONE task is clicked it should be marked as DONE
+ * - When DONE task is clicked it should be marked as NOT DONE
+ * - Task marked as DONE should have a strikethrough over text (see CSS)
+ */
+
+
 //--------------------------- TODO ITEM MODEL -------------------------
 export type TodoItem = {
     task: string;
@@ -8,6 +32,9 @@ export type TodoItem = {
   }
 //---------------------------------------------------------------------
 
+
+
+//------------------------- TODO LIST COMPONENT -----------------------
 @Component({
     selector: 'app-todo-list',
     standalone: true,
@@ -17,7 +44,7 @@ export type TodoItem = {
     <a routerLink="../expected">Show expected outcome</a>
     
     <form style="padding-top: 1rem">
-        <input type="text" placeholder="Todo Name"/>
+        <input type="text" placeholder="Task Name"/>
         <button type="button">Add</button>
     </form>
 
@@ -34,3 +61,4 @@ export type TodoItem = {
 export class TodoListComponent {
     items: TodoItem[] = [];
 }
+//---------------------------------------------------------------------
